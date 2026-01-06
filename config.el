4e339ff05aacac41105e539127d750518e578a3c
Abilkhair Seilov <abilkhairseilov@outlook.com>
2026-01-06 22:21:24 +0300

    [feat] enable nix and change font

--- config.el
+++ config.el
@@ -21,7 +21,7 @@
 ;; accept. For example:
 ;;
 (setq doom-font (font-spec :family "JetBrainsMono Nerd Font" :size 20 :weight 'semi-light)
-     doom-variable-pitch-font (font-spec :family "Fira Sans" :size 21))
+     doom-variable-pitch-font (font-spec :family "Noto Sans" :size 21))
 ;;
 ;; If you or Emacs can't find your font, use 'M-x describe-font' to look them
 ;; up, `M-x eval-region' to execute elisp code, and 'M-x doom/reload-font' to
