50a23e711e066dd448718a1a82645c35ac072660
Abilkhair Seilov <abilkhairseilov@outlook.com>
2026-01-19 16:41:21 +0300

    [feat] update templates

--- config.el
+++ config.el
@@ -31,7 +31,7 @@
 ;; There are two ways to load a theme. Both assume the theme is installed and
 ;; available. You can either set `doom-theme' or manually load a theme with the
 ;; `load-theme' function. This is the default:
-(setq doom-theme 'doom-bluloco-dark)
+(setq doom-theme 'doom-dark+)
 
 ;; This determines the style of line numbers in effect. If set to `nil', line
 ;; numbers are disabled. For relative line numbers, set this to `relative'.
@@ -92,6 +92,9 @@
                    ("S" "Subjects")
                    ("Sc" "Chemistry" entry
                     (file+headline "workspace/a-level/chemistry.org" "Chemistry")
+                    "* %U\nTopic: %?\n- %i")
+                   ("Sp" "Computer science" entry
+                    (file+headline "workspace/a-level/computerscience.org" "Computer Science")
                     "* %U\nTopic: %?\n- %i")
                   )
     )
